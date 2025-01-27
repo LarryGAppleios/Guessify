34a75cca0823c4c6b9994e67dac25ac53e657e36
Larry Gill <larrygill909@gmail.com>
2025-01-27 10:56:58 -0500

    Initial Commit

---
+++ Guessify/GuessifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessifyApp.swift
+//  Guessify
+//
+//  Created by Larry Gill on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessifyTests/GuessifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessifyTests.swift
+//  GuessifyTests
+//
+//  Created by Larry Gill on 1/27/25.
+//
+
+import Testing
+@testable import Guessify
+
+struct GuessifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
