f1fe04b4366860713249fafab076d46314ec892a
Larry Gill <larrygill909@gmail.com>
2025-02-05 11:43:37 -0500

    chnaged the scroll view layout in gameview

--- Guessify/Testing.swift
+++ Guessify/Testing.swift
@@ -6,13 +6,56 @@
 //
 
 import SwiftUI
-
-struct Testing: View {
+struct KeyboardView: View {
+    @State var proxy: UITextDocumentProxy
+    
+    let keys = [
+        ["Q","W","E","R","T","Y","U","I","O","P"],
+        ["A","S","D","F","G","H","J","K","L"],
+        ["Z","X","C","V","B","N","M"]
+    ]
+    //    @State private var inputText: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ForEach(keys, id: \.self) { row in
+                HStack{
+                    ForEach(row, id: \.self) { key in
+                        KeyButton(title: key, proxy: proxy)
+                    }
+                }
+            }
+        }
+        .padding()
     }
 }
+struct KeyButton: View {
+    var title: String
+    var proxy: UITextDocumentProxy
+    var body: some View {
+        Button(action: {
+            proxy.insertText(title)
+        }) {
+            Text(title)
+                .frame(width: 40, height: 40)
+                .background(Color.gray.opacity(0.5))
+                .cornerRadius(5)
+        }
+    }
+    func insertText(_ text: String) {
+        proxy.insertText(text)
+    }
 
+       
+        
+//        TextField("Text Input", text: $inputText, prompt: Text("Input Text"), axis: .vertical)
+//            .textFieldStyle(.automatic)
+//            .textContentType(.name)
+//            .textInputAutocapitalization(.never)
+//            .disableAutocorrection(true)
+//            .lineLimit(3, reservesSpace: false)
+//    }
+    
+}
 #Preview {
-    Testing()
+//    Testing()
 }
