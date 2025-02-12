ffaf31b66ab3b7fafde2b722a206d3c2ab0beb2e
Otis YII <oyoung21@gmail.com>
2025-02-12 08:36:40 -0500

    Finished Guessed remaing the game functions as it should. 4 rounds all using the words we desiated.

--- Guessify/AcroynmLogic.swift
+++ Guessify/AcroynmLogic.swift
@@ -7,132 +7,108 @@
 
 
 import SwiftUI
- 
-
-
 
 struct AcronymLogic: View {
     
-    let acronymWords: [String] = ["ASAP", "BTW", "FYI", "IDK", "IMO", "LOL", "NVM", "WWJD", "YOLO", "ROFL", "OMG", "JFYI", "TYT", "SMH", "BFF", "DIY", "ETA", "TBA", "TBD", "TGIF", "WTG", "NSFW", "IYKYK", "FOMO", "BRB", "AMA", "CEO", "EOD", "EOY", "NDA", "ROI", "CFO", "UI", "VPN","GPA", "ESL", "STEM", "ADHD", "AIDS", "BPM", "DNA", "ICU", "RNA", "ACT"]
+    let acronymWords: [String] = ["ASAP", "BTW", "FYI", "IDK", "IMO", "LOL", "NVM", "WWJD", "YOLO", "ROFL", "OMG", "JFYI", "TYT", "SMH", "BFF", "DIY", "ETA", "TBA", "TBD", "TGIF", "WTG", "NSFW", "IYKYK", "FOMO", "BRB", "AMA", "CEO", "EOD", "EOY", "NDA", "ROI", "CFO", "UI", "VPN", "GPA", "ESL", "STEM", "ADHD", "AIDS", "BPM", "DNA", "ICU", "RNA", "ACT"]
     
-    // Choosing a random acronym from the list
     @State private var currentAcronym: String = ""
     @State private var guessedLetters: Set<Character> = []
     @State private var correctGuesses: Set<Character> = []
     @State private var wrongGuesses: Int = 0
-    @State private var guessesSoFar: Int = 0
     @State private var guessesRemaining: Int = 6
     @State private var currentHint: String = ""
     @State private var currentRound: Int = 1
     
-//  Connects correct acronym to the correct hint.
-        let hintDictionary:[String: String] = [
-            "ASAP" : "As Fast as you can.",
-            "BTW" : "Used to introduce a side note in conversation.",
-            "FYI" : "Providing Information that may be useful.",
-            "IDK" : "Expressing uncertainty.",
-            "IMO" : "Stating an opinion.",
-            "LOL" : "Reaction to something funny.",
-            "NVM" : "Telling someone to disregard what was said.",
-            "WWJD" : "Religious phrase.",
-            "YOLO" : "Encouragement to take risks.",
-            "ROFL" : "Something is extremely funny.",
-            "OMG" : "Expressing suprise or shock.",
-            "JFYI" : "Another way to say just so you know.",
-            "TYT" : "Don't rush.",
-            "SMH" : "Disappointed expression.",
-            // Round 2
-            "BFF" : "Your friend til the end.",
-            "DIY" : "Craft it yourself, no store needed!",
-            "ETA" : "How long until arrival ?",
-            "TBA" : "Details are coming soon.",
-            "TBD" : "Not decided yet.",
-            "TGIF" : "Weekend starts now.",
-            "WTG" : "Great job, keep it up!",
-            "NSFW" : "Don't open this at work.",
-            "IYKYK" : "An inside joke for those who know.",
-            "FOMO" : "Don't want to miss out",
-            "BRB" : "Gone for a sec.",
-            "AMA" : "Ask whatever.",
-            // Round 3
-            "CEO" : "The big boss in charge!",
-            "EOD" : "Deadline by tonight.",
-            "EOY" : "Wrapping up the year.",
-            "NDA" : "Keep this a secret.",
-            "ROI" : "What's the profit ?",
-            "CFO" : "Handles all the money.",
-            "UI" : "What you see on screen!",
-            "VPN" : "Browse safely and privately.",
-            "GPA" : "Your academic score.",
-            // Round 4
-            "ESL" : "Learning English.",
-            "STEM" : "A class involving 4 subjects.",
-            "ADHD" : "A brain that loves to multitask.",
-            "AIDS" : "A serious immune system disease.",
-            "BPM" : "Heartbeats or music tempo.",
-            "DNA" : "Your genetic blueprint.",
-            "ICU" : "Where critical care happens!",
-            "RNA" : "DNA's messenger helper.",
-            "ACT" : "A big test for college."]
-            
+    let hintDictionary: [String: String] = [
+        "ASAP" : "As Fast as you can.",
+        "BTW" : "Used to introduce a side note in conversation.",
+        "FYI" : "Providing Information that may be useful.",
+        "IDK" : "Expressing uncertainty.",
+        "IMO" : "Stating an opinion.",
+        "LOL" : "Reaction to something funny.",
+        "NVM" : "Telling someone to disregard what was said.",
+        "WWJD" : "Religious phrase.",
+        "YOLO" : "Encouragement to take risks.",
+        "ROFL" : "Something is extremely funny.",
+        "OMG" : "Expressing suprise or shock.",
+        "JFYI" : "Another way to say just so you know.",
+        "TYT" : "Don't rush.",
+        "SMH" : "Disappointed expression.",
+        "BFF" : "Your friend til the end.",
+        "DIY" : "Craft it yourself, no store needed!",
+        "ETA" : "How long until arrival ?",
+        "TBA" : "Details are coming soon.",
+        "TBD" : "Not decided yet.",
+        "TGIF" : "Weekend starts now.",
+        "WTG" : "Great job, keep it up!",
+        "NSFW" : "Don't open this at work.",
+        "IYKYK" : "An inside joke for those who know.",
+        "FOMO" : "Don't want to miss out",
+        "BRB" : "Gone for a sec.",
+        "AMA" : "Ask whatever.",
+        "CEO" : "The big boss in charge!",
+        "EOD" : "Deadline by tonight.",
+        "EOY" : "Wrapping up the year.",
+        "NDA" : "Keep this a secret.",
+        "ROI" : "What's the profit ?",
+        "CFO" : "Handles all the money.",
+        "UI" : "What you see on screen!",
+        "VPN" : "Browse safely and privately.",
+        "GPA" : "Your academic score.",
+        "ESL" : "Learning English.",
+        "STEM" : "A class involving 4 subjects.",
+        "ADHD" : "A brain that loves to multitask.",
+        "AIDS" : "A serious immune system disease.",
+        "BPM" : "Heartbeats or music tempo.",
+        "DNA" : "Your genetic blueprint.",
+        "ICU" : "Where critical care happens!",
+        "RNA" : "DNA's messenger helper.",
+        "ACT" : "A big test for college."
+    ]
     
     let maxGuesses: Int = 6
-
-    // Function to show the acronym with guessed letters
+    
     func acronymDisplay() -> String {
-        var display = " "
+        var display = ""
         for letter in currentAcronym {
             if correctGuesses.contains(letter) {
                 display += "\(letter)"
             } else {
-                display += " _ "
+                display += "_ "
             }
         }
         return display
     }
-
-    // Function to start a new game
-    func startNewGame() {
-    if currentRound > 4 {
-        currentRound = 1 // reset to 1 after 4 rounds.
-        //If we've reached reound 5 (or the number of rounds desinated
-        currentAcronym = acronymWords.randomElement() ?? "ASAP"
-        currentHint = ""
-        
-    }
     
+    func startNewGame() {
+        if currentRound > 4 {
+            currentRound = 1 // Reset to Round 1 after 4 rounds
+        }
         
-        // Hint Logic
+        let roundAcronyms = getAcronymsForRound(round: currentRound)
+        currentAcronym = roundAcronyms.randomElement() ?? "ASAP"
         currentHint = hintDictionary[currentAcronym] ?? "No hint available"
         
         guessedLetters.removeAll()
         correctGuesses.removeAll()
         wrongGuesses = 0
         guessesRemaining = maxGuesses
-
     }
     
-    // Function to handle a guess from one player
     func player1Guess(letter: Character) {
         if guessedLetters.contains(letter) {
-            // Already guessed this letter, return early
-            return
+            return // Already guessed this letter, return early
         }
-        guessedLetters.insert(letter)
-        //Select the right set of Acronyms based on round
-       let roundAcronyms = getAcronymsForRound(round: currentRound)
-        currentAcronym = roundAcronyms.randomElement() ?? "ASAP"
+        
         guessedLetters.insert(letter)
         
         if currentAcronym.contains(letter) {
             correctGuesses.insert(letter)
         } else {
             wrongGuesses += 1
-            guessesSoFar += 1
             guessesRemaining -= 1
         }
-    
-        guessesSoFar += 1
     }
     
     func getAcronymsForRound(round: Int) -> [String] {
@@ -149,6 +125,7 @@ struct AcronymLogic: View {
             return []
         }
     }
+    
     var body: some View {
         VStack {
             Text("Acronym Guessing Game")
@@ -156,6 +133,7 @@ struct AcronymLogic: View {
                 .padding()
                 .shadow(color: .yellow, radius: 12)
                 .foregroundColor(.white)
+            
             Text("Guess the acronym:")
                 .font(.body)
                 .foregroundColor(.white)
@@ -166,16 +144,20 @@ struct AcronymLogic: View {
                 .shadow(color: .yellow, radius: 12)
                 .fixedSize(horizontal: false, vertical: true)
             
-            // Display the acronym with underscores for unguessed letters
             Text(acronymDisplay())
                 .font(.largeTitle)
                 .padding()
                 .foregroundColor(.white)
-            // Show how many guesses are remaining
+            
             Text("Guesses Remaining: \(guessesRemaining)")
                 .padding()
                 .foregroundColor(.white)
-            // Input for the user's guess (a single letter)
+            Text("Round \(currentRound)")
+                .font(.title2.bold())
+                .padding()
+                .shadow(color: .yellow, radius: 12)
+                .foregroundColor(.white)
+            
             let columns = [GridItem(.adaptive(minimum: 40))]
             ScrollView {
                 LazyVGrid(columns: columns, spacing: 10) {
@@ -190,8 +172,6 @@ struct AcronymLogic: View {
                                 .background(guessedLetters.contains(letter) ? Color.gray : Color.yellow)
                                 .foregroundColor(.white)
                                 .cornerRadius(5)
-                            
-                            
                         }
                         .disabled(guessedLetters.contains(letter))
                     }
@@ -199,51 +179,50 @@ struct AcronymLogic: View {
             }
             .frame(height: 290)
             .padding()
-
-            // Show the result (if game over)
-            if guessesRemaining <= 0 || currentAcronym.allSatisfy({!correctGuesses.contains($0)}){
-                Text(guessesRemaining <= 0 ? "Game Over" : "You Lose")
+            
+            // If the game is over
+            if guessesRemaining <= 0 {
+                Text("Game Over")
                     .font(.title)
                     .padding()
                     .foregroundColor(.white)
                 Button("Start New Game") {
-                    currentRound += 1
+                    currentRound = 1 // Restart from Round 1
                     startNewGame()
-                    
                 }
-                
                 .padding()
-            } else if currentAcronym.allSatisfy({correctGuesses.contains($0)}) {
-                
-                // Player guesses the Acronym Correctly, move to the next round
+            } else if currentRound == 4 && currentAcronym.allSatisfy({ correctGuesses.contains($0) }) {
+                // Display winning message only on round 4
                 Text("You Win Guessify!")
                     .font(.title)
                     .padding()
                     .foregroundColor(.white)
-                Button("Next Round") {
+                Button("Start New Game") {
+                    currentRound = 1 // Restart from Round 1
                     startNewGame()
                 }
                 .padding()
+            } else if currentAcronym.allSatisfy({ correctGuesses.contains($0) }) {
+                // If the user guesses the acronym correctly before round 4
+                Text("You Win The Round")
+                    .font(.title)
+                    .padding()
+                    .foregroundColor(.white)
+                Button("Next Round") {
+                    currentRound += 1
+                    startNewGame()
+                }
+
             }
         }
         .onAppear {
-            
             startNewGame() // Start the game when the view appears
         }
-      
-            
-        }
     }
+}
 
-
-struct AcroynmLogic: View {
+struct AcronymLogicView: View {
     var body: some View {
-        
-        
-        
-        
         AcronymLogic()
     }
 }
-        
-
